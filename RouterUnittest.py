b90629a834e4cd5487ca667340e2a3818847da3c
CokeFung <naruebetcm@gmail.com>
2021-02-05 13:36:34 +0000

    add Unittest - addd get interface test case

--- RouterUnittest.py
+++ RouterUnittest.py
@@ -8,7 +8,7 @@ r1 = Router(hostname, brand, model)
 
 class TestRouter(unittest.TestCase):
         
-    def test_basicMethods(self):
+    def test_01_basicMethods(self):
         newHostname = "R1"
         newBrand = "Cisco"
         newModel = "Catalyst 8300-1N1S-4T2X"
@@ -19,12 +19,16 @@ class TestRouter(unittest.TestCase):
         self.assertTrue(r1.getBrand() == newBrand)
         self.assertTrue(r1.getModel() == newModel)
 
-    def test_addInterface(self):
-        int_name = "g0/0"
-        is_shutdown = True
-        self.assertTrue(r1.add_interface(int_name, is_shutdown))     # add new interface
-        self.assertFalse(r1.add_interface(int_name, is_shutdown))    # interface already existed
+    def test_02_addInterface(self):
+        intname = "g0/0"
+        self.assertTrue(r1.add_interface(intname))     # add new interface
+        self.assertFalse(r1.add_interface(intname))    # interface already exists
 
+    def test_03_getInterface(self):
+        intname1 = "g0/0"
+        intname2 = "g0/1"
+        self.assertFalse(r1.get_interface(intname1) == {})     # found interface
+        self.assertTrue(r1.get_interface(intname2) == {})      # not found interface  
 
 if __name__ == "__main__":
     unittest.main()
