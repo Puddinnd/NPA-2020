a79f06180a5b7c825521d4e74223bc49f8389494
CokeFung <naruebetcm@gmail.com>
2021-02-05 10:56:33 +0000

    add Unittest - add interface method

--- RouterUnittest.py
+++ RouterUnittest.py
@@ -14,10 +14,15 @@ class TestRouter(unittest.TestCase):
         newModel = "Catalyst 8300-1N1S-4T2X"
         r1.setHostname(newHostname)
         r1.setBrand(newBrand)
-        r1.setModel(newModel)
+        r1.setModel(newModel)       
         self.assertTrue(r1.getHostname() == newHostname)
         self.assertTrue(r1.getBrand() == newBrand)
         self.assertTrue(r1.getModel() == newModel)
+
+    def test_addInterface(self):
+        int_name = "g0/0"
+        self.assertTrue(r1.add_interface(int_name))     # add new interface
+        self.assertFalse(r1.add_interface(int_name))    # interface already existed
 
 
 if __name__ == "__main__":
