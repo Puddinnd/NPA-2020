3d854b7e0032ef30ef7135ebdd48213aff8f85f6
CokeFung <naruebetcm@gmail.com>
2021-02-05 10:35:47 +0000

    change Unittest - createObject to basic methods

--- RouterUnittest.py
+++ RouterUnittest.py
@@ -1,16 +1,23 @@
 from RouterClass import Router
 import unittest
 
-class TestRouter(unittest.TestCase):
+hostname = "temp hostname"
+brand = "temp brand"
+model = "temp model"
+r1 = Router(hostname, brand, model)
 
-    def test_createObject(self):
-        hostname = "R1"
-        brand = "Cisco"
-        model = "Catalyst 8300-1N1S-4T2X"
-        r1 = Router(hostname, brand, model)
-        self.assertTrue(r1.getHostname() == hostname)
-        self.assertTrue(r1.getBrand() == brand)
-        self.assertTrue(r1.getModel() == model)
+class TestRouter(unittest.TestCase):
+        
+    def test_basicMethods(self):
+        newHostname = "R1"
+        newBrand = "Cisco"
+        newModel = "Catalyst 8300-1N1S-4T2X"
+        r1.setHostname(newHostname)
+        r1.setBrand(newBrand)
+        r1.setModel(newModel)
+        self.assertTrue(r1.getHostname() == newHostname)
+        self.assertTrue(r1.getBrand() == newBrand)
+        self.assertTrue(r1.getModel() == newModel)
 
 
 if __name__ == "__main__":
