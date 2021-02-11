8103fccdf71699cf6cd0a60dcec84e2838760f27
CokeFung <naruebetcm@gmail.com>
2021-02-11 19:20:27 +0000

    add sent_config - implement setIPtoInterface function

--- week4-Netmiko/sent_config.py
+++ week4-Netmiko/sent_config.py
@@ -1,16 +1,36 @@
 from netmiko import ConnectHandler
 
-device_ip = "172.31.178.1"
-username = "admin"
-password = "cisco"
+y = 178
+x = 8
 
-device_params = {
-    "device_type": "cisco_ios",
-    "ip": device_ip,
-    "username": username,
-    "password": password,
-}
+def setIPtoInterface(device_params, interface, ip, subnet):
+    with ConnectHandler(**device_params) as ssh:
+        ssh.config_mode()
+        commands = [
+            "int {}".format(interface),
+            "ip add {} {}".format(ip, subnet),
+            "no shut",
+        ]
+        ssh.send_config_set(commands)
+        ssh.exit_config_mode()
+        ssh.send_command('write mem')
+        result = ssh.send_command('sh ip int br')
+        ssh.disconnect()
+        return result
 
-with ConnectHandler(**device_params) as ssh:
-    result = ssh.send_command('sh ip int br')
+def main():
+    device_ip = "172.31.178.1"
+    username = "admin"
+    password = "cisco"
+
+    device_params = {
+        "device_type": "cisco_ios",
+        "ip": device_ip,
+        "username": username,
+        "password": password,
+    }
+    result = setIPtoInterface(device_params, "lo0", "172.20.178.1", "255.255.255.255")
     print(result)
+
+    
+main()
