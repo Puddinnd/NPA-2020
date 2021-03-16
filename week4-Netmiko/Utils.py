09dbf166cf6e860e0b6ec703166870225239b147
NetAcad DEVASC <changeme@example.com>
2021-03-16 16:52:43 +0000

    update config_devices - add loopback, NAT and advertise default route (R5)

--- week4-Netmiko/Utils.py
+++ week4-Netmiko/Utils.py
@@ -20,26 +20,55 @@ def delay_print(s, newline):
     for c in s:
         sys.stdout.write(c)
         sys.stdout.flush()
-        time.sleep(0.05)
+        time.sleep(0.03)
     if newline:
         print()
     else:
         backline()
+################################################################################################################
+####################################### Configure Loopback interface ###########################################
+def configLoopback():
+    print("Start configuring loopback interface to all devices...")
+    devices_management = yaml.load(open(config_path + "devices_management_info.yml"), Loader=yaml.Loader)
+    for dm in devices_management:
+        delay_print("[*] Configuring: " + dm['name'] + "'s loopback", False)
+        configIP2loopback(dm)
+        delay_print("[+] Completed: " + dm['name'] + "'s loopback is up", True)
+
+def configIP2loopback(device):
+    loopback_nw = "172.20.178."
+    loopback_sn = "255.255.255.255"
+    device_params = {
+        "device_type": "cisco_ios",
+        "ip": device['management_ip'],
+        "username": username,
+        "password": password,
+    }
+    with ConnectHandler(**device_params) as ssh:
+        ssh.config_mode()
+        ssh.send_command("int lo 0", expect_string=r"#")
+        ssh.send_command("ip address {}{} {}".format(loopback_nw, device['id'], loopback_sn), expect_string=r"#")
+        ssh.send_command("no shut", expect_string=r"#")
+        ssh.disconnect()
 
 ################################################################################################################
 ########################################## Configure IP addresss ###############################################
 def configInterfaces():
     print("Start configuring IP addresses to all devices...")
     devices_config = yaml.load(open(config_path + "devices_interface_info.yml"), Loader=yaml.Loader)
+    devices_management = yaml.load(open(config_path + "devices_management_info.yml"), Loader=yaml.Loader)
     # pp.pprint(devices_config)
+    i = 0
     for device in devices_config:
         delay_print("[*] Verifying: " + device['name'], False)
+        device['management_ip'] = devices_management[i]['management_ip']
         status = verifyDeviceInfo(device)
         if status:
             delay_print("[+] "+device['name'] + " status: [OK]                  ", True)
         else:
             delay_print("[-] "+device['name'] + " status: [Fixed]               ", True)
         # break
+        i += 1
     print()
 
 def verifyDeviceInfo(device):
@@ -142,6 +171,54 @@ def addACLtoDevice(management_ip, acl_list):
                     ssh.send_command("exit", expect_string=r"#")
         ssh.disconnect()
 
+################################################################################################################
+############################################# Configure OSPF ###################################################
+def configOSPF():
+    print("Start to configure OSPF on all Data/Control plane interfaces...")
+    devices_config = yaml.load(open(config_path + "devices_interface_info.yml"), Loader=yaml.Loader)
+    devices_mangement = yaml.load(open(config_path + "devices_management_info.yml"), Loader=yaml.Loader)
+    for i in range(len(devices_config)):
+        if 'S' in devices_config[i]['name']:
+            continue
+        delay_print("[*] Configuring OSPF in {}".format(devices_config[i]['name']), False)
+        addOSPF(devices_mangement[i]['management_ip'], devices_config[i])
+        delay_print("[+] {} is advertising network information to other devices".format(devices_config[i]['name']), True)
+
+def addOSPF(management_ip, devcon):
+    device_params = {
+        "device_type": "cisco_ios",
+        "ip": management_ip,
+        "username": username,
+        "password": password,
+    }
+    networks = getNetworkfrominterface(devcon)
+    with ConnectHandler(**device_params) as ssh:
+        ssh.enable()
+        ssh.config_mode()
+        ssh.send_command("router ospf 1", expect_string=r"#")
+        for nw in networks:
+            ssh.send_command("network {} {} area 0".format(nw[0], nw[1]), expect_string=r"#")
+        ssh.disconnect()
+
+def getNetworkfrominterface(devcon):
+    nw = []
+    nw.append(["172.31.178.0", "0.0.0.15"])
+    if devcon['loopback']:
+        nw.append(["172.20.178." + str(devcon['id']), "0.0.0.0"])
+    if devcon['interfaces']:
+        for intf in devcon['interfaces']:
+            nw_ip = [int(i) for i in intf['ip'].split('.')]
+            nw_nm = [int(i) for i in intf['subnet'].split('.')]
+            tmp = str(nw_ip[0]&nw_nm[0]) + '.' +\
+                  str(nw_ip[1]&nw_nm[1]) + '.' +\
+                  str(nw_ip[2]&nw_nm[2]) + '.' +\
+                  str(nw_ip[3]&nw_nm[3])
+            nw.append([
+                tmp,
+                intf['wildcard']
+            ])
+    # pp.pprint(nw)
+    return nw
 
 ################################################################################################################
 ########################################## Configure CDP and LLDP ##############################################
@@ -167,7 +244,6 @@ def enableCDP(device_ip):
         ssh.config_mode()
         ssh.send_command('cdp run', expect_string=r"#")
         ssh.disconnect()
-
 
 def enableLLDP(device_ip):
     device_params = {
@@ -236,4 +312,37 @@ def addDescription2Interfaces(device_ip, cdp_data):
             ssh.send_command("description \"connect to {} of {}\"".format(data[1], data[2]), expect_string=r"#")
             ssh.send_command("exit", expect_string=r"#")
         ssh.disconnect()
-            
+
+################################################################################################################
+############################### Configure NAT and advertise default route (R5) #################################
+def NATnDefaultRoute2R5():
+    device_params = {
+        "device_type": "cisco_ios",
+        "ip": "172.31.178.9",
+        "username": username,
+        "password": password,
+    }
+    with ConnectHandler(**device_params) as ssh:
+        ssh.enable()
+        ssh.config_mode()
+        ### advertise default route from R5
+        delay_print("[+] Advertise default route from R5", True)
+        ssh.send_command('router ospf 1', expect_string=r"#")
+        ssh.send_command('default-information originate', expect_string=r"#")
+        ssh.send_command('exit', expect_string=r"#")
+        ### config PAT to interface G0/2 of R5
+        delay_print("[*] Configuring NAT on R5", False)
+        ssh.send_command('int g0/2', expect_string=r"#")
+        ssh.send_command('ip nat enable', expect_string=r"#")
+        ssh.send_command('ip nat outside', expect_string=r"#")
+        ssh.send_command('int g0/1', expect_string=r"#")
+        ssh.send_command('ip nat enable', expect_string=r"#")
+        ssh.send_command('ip nat inside', expect_string=r"#")
+        ssh.send_command('int g0/0', expect_string=r"#")
+        ssh.send_command('ip nat enable', expect_string=r"#")
+        ssh.send_command('ip nat inside', expect_string=r"#")
+        ssh.send_command('exit', expect_string=r"#")
+        ssh.send_command('access-list 1 permit any', expect_string=r"#")
+        ssh.send_command('ip nat inside source list 1 interface g0/2 overload', expect_string=r"#")
+        delay_print("[+] NAT on R5 is enabled      ", False)
+        ssh.disconnect()
