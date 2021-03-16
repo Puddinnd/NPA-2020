7144e3e523092204a3ecc85cd9f2b1d6bd021964
NetAcad DEVASC <changeme@example.com>
2021-03-16 16:54:03 +0000

    update connfig - add loopback and NAT options

--- week4-Netmiko/config_devices.py
+++ week4-Netmiko/config_devices.py
@@ -4,24 +4,20 @@ def main():
     print("_______________________________________________")
     print("|                                              |")
     print("|  Select an options to configure:             |")
-    print("|   [0]: all                                   |")
+    print("|   [0]: loopback IP address                   |")
     print("|   [1]: IP addresses                          |")
     print("|   [2]: access-lists                          |")
     print("|   [3]: OSPF                                  |")
     print("|   [4]: enable CDP and LLDP                   |")
     print("|   [5]: description based on CDP              |")
     print("|   [6]: NAT and advertise default route (R5)  |")
-    # print("|                                 |")
-    # print("|  use comma to select multiple   |")
-    # print("|  like: 1,2                      |")
     print("|______________________________________________|")
     print()
     option = input("Choose: ").strip()
     print()
 
-    if option == 0:
-        configInterfaces()
-        configACL()
+    if option == "0":
+        configLoopback()
     elif option == "1":
         configInterfaces()
     elif option == "2":
