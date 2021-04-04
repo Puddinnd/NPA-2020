9f3bfd374ef2519de5b45769cbbe2c69403f17fe
CokeFung <naruebetcm@gmail.com>
2021-04-04 17:19:17 +0000

    update Secret - add youtube and spotify secret

--- week6-REST_API/bot/api-chat-bot/Secret-template.py
+++ week6-REST_API/bot/api-chat-bot/Secret-template.py
@@ -3,10 +3,18 @@ public_url = "http://xxx.ngrok.io" # `ngrok http 5000`
 bot_token = "..."
 my_token = "..."
 mapquest_token = "..."
-selected_room_id = "..." # room in webex team
+selected_room_id = "..." # RoomID in webex team
 webhook_spec = {
     "resource": "messages",
     "event": "created",
     "targetUrl": public_url,
     "name": "WebHook - TEST BOT room",
 }
+
+## Youtube 
+youtube_api_service_name = "youtube"
+youtube_api_version = "v3"
+youtube_DEVELOPER_KEY = "..."
+### Spotify
+spotify_client_id = "..."
+spotify_client_secret = "..."
